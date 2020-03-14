f1848fdd5522dc00b8e42482c76e127298f58ab8
ahmadrosid <alahmadrosid@gmail.com>
2020-03-14 17:10:34 +0700

    fix zhrc path

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-ZSHRC=$HOME/.config/zsh/.zshrc
+ZSHRC=$HOME/.zshrc
 if [ -f "$ZSHRC" ]; then
+    if [ -f "$HOME/.config/zsh/.zshrc" ];  then
+	ZSHRC=$HOME/.config/zsh/.zshrc
+    fi
     echo "export OCITRC_DIR=$(pwd)" >> $ZSHRC
     echo "source $(pwd)/.ocitrc" >> $ZSHRC
-    OCITRC_DIR=$(pwd) source $(pwd)/.ocitrc
     echo "Dotfiles installed successfuly."
 else
     echo "Please install zsh! Visit https://www.zsh.org"
